0a3ad3885310abf42e31a9f936a34bfe35da21cf
wagner-rodri <luan.frra@gmail.com>
2020-10-12 12:31:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python
+ Exercícios em python
---
+++ practical-programming-Al-Sweigart/lesson-2-If-else-elif-statement/if-elif-else-exemple.py
@@ -0,0 +1,9 @@
+name = 'Alice'
+age = 3000
+
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print ('You are not Alice, kiddo!')
+else:
+    print ('done')
