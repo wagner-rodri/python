a753b2fc80f6a2c92e548951a2b44cc1b3a3d6a5
wagner-rodri <luan.frra@gmail.com>
2020-10-16 11:01:03 -0300

    Tipos de dados
    Exemplificado alguns dos tipos de dados em Python.

--- the-basics/basics.py
+++ the-basics/basics.py
@@ -6,3 +6,15 @@ myNumber = 28
 myText = 'Hello'
 
 print(myNumber, myText)
+
+x = 10
+y = '18' 
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
+
+student_grades = [9.1, 8.8, 7.5]
