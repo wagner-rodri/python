a19bcdd13d87d7521df0f3553cf7ab935737c439
wagner-rodri <luan.frra@gmail.com>
2020-11-06 12:16:06 -0300

    Data validation - checking user input

--- english-thesaurus/app1.py
+++ english-thesaurus/app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("python/english-thesaurus/data.json"))
 
@@ -7,7 +8,15 @@ word = input('Enter a word: ').lower()
 def translate(word):
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yN = input('Did you mean %s instead?[Y or N]: ' % get_close_matches(word, data.keys())[0]).upper()
+        if yN == 'Y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yN == "N":
+            return ("The word doesn't exist, Please double check it!")
+        else:
+            return ("We didn't understand your entry. Please try again!")
     else:
-        return("The word doesn't exist, Please double check it!")
+        return ("The word doesn't exist, Please double check it!")
 
 print(translate(word))
